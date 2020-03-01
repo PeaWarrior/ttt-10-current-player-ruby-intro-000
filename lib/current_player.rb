ab9cae803984085f4adfd73c4f95820019b2adb4
Jackson Chen <jacksonchen171@gmail.com>
2020-03-01 19:45:38 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count (board)
   count = 0
-  board.each do |played|
-    if played == "X" || played == "O"
+  board.each do |square|
+    if square == "X" || square == "O"
       count += 1
   end
   return count
